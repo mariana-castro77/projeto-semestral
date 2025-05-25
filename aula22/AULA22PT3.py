24884591567406a66c888609ac7cea749d15fb7c
mariana-castro77 <marianadecastrorodrigues@gmail.com>
2025-05-25 16:55:48 -0300

    :tada: Commit Inicial

--- aula22/AULA22PT3.py
+++
@@ -1,8 +0,0 @@
-def CalcularMedia (n1,n2):
-    media = (n1 + n2)/2
-    return round(media,2)
-
-nota1 = float(input("Digite a nota 1: "))
-nota2 = float(input("Digite a nota 2: "))
-
-mediaFinal = CalcularMedia(nota1,nota2)
